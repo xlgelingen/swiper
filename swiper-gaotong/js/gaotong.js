29c50cf979e5618b05a2ddcd45549bab23b96b35
vousreussirez <xuliucg@gmail.com>
2023-11-28 23:23:04 +0800

    add swiper to gaotong-task

---
+++ swiper-gaotong/js/gaotong.js
@@ -0,0 +1,300 @@
+class Swiper {
+    constructor() {
+        this.data = {
+            index: 0,
+            duration: 500,
+            isLock: false,
+            translateX: 0,
+            defaultLength: null,
+            listWidth: null,
+            itemWidth: null,
+            itemMargin: 0,
+            timer: null,
+        }
+
+        this.nextArrow = this.nextArrow.bind(this);
+        this.preArrow = this.preArrow.bind(this);
+        this.init = this.init.bind(this);
+        this.playTime = this.playTime.bind(this);
+        this.pauseScroll = this.pauseScroll.bind(this);
+
+        this.init();
+        this.bind();
+    }
+
+    bind() {
+        let preArrow = document.getElementById('pre-arrow');
+        preArrow.addEventListener('click', this.preArrow);
+
+        let nextArrow = document.getElementById('next-arrow');
+        nextArrow.addEventListener('click', this.nextArrow);
+
+        //每1.5s自动切换，当鼠标移动到teacher-carousel里面，停止自动切换，当鼠标移出时，恢复自动切换
+        let teacherCarousel = document.getElementById('teacher-carousel');
+        teacherCarousel.addEventListener("mouseenter", this.pauseScroll);
+        teacherCarousel.addEventListener("mouseleave", this.playTime);
+
+        window.addEventListener('resize', this.init);
+    }
+
+    init() {
+        this.pauseScroll();
+
+        let teacherCardList = document.getElementById('teacher-card-list');
+        let teacherCards = document.getElementsByClassName('teacher-card');
+        let listWidth = teacherCardList.offsetWidth;
+        let cardItemWidth = teacherCards[0].offsetWidth;
+        let itemMargin = (listWidth - 4 * cardItemWidth) / 3;
+        for (let item of teacherCards) {
+            item.style.marginRight = itemMargin + 'px';
+        }
+        let index = this.data.index;
+        let translateX = - ((cardItemWidth + itemMargin) * index);
+        this.data.defaultLength = teacherCards.length - 4;
+        this.data.itemWidth = cardItemWidth;
+        this.data.itemMargin = itemMargin;
+        this.data.translateX = translateX;
+        teacherCardList.style.transform = `translateX(${translateX}px)`;
+
+        this.playTime();
+    }
+
+    animateTo(begin, end, duration, changeCallback, finishCallback) {
+        let startTime = Date.now();
+        let self = this;  // 将 this 存储在变量中
+        requestAnimationFrame(function update() {
+            let dateNow = Date.now();
+            let time = dateNow - startTime;
+            let value = self.linear(time, begin, end, duration);
+            typeof changeCallback == 'function' && changeCallback(value);
+
+            if (startTime + duration > dateNow) {
+                requestAnimationFrame(update);
+            } else {
+                typeof finishCallback === 'function' && finishCallback(end);
+            }
+        });
+    }
+
+    linear(time, begin, end, duration) {
+        return (end - begin) * time / duration + begin;
+    }
+
+    goIndex(index) {
+        let swiperDuration = this.data.duration;
+        let cardItemWidth = this.data.itemWidth;
+        let swiperLength = this.data.defaultLength;
+        let preArrow = document.getElementById('pre-arrow');
+        let nextArrow = document.getElementById('next-arrow');
+        if (index <= -1) {
+            index = swiperLength;
+        }
+        if (index >= swiperLength + 1) {
+            index = 0;
+        }
+
+
+        let itemMargin = this.data.itemMargin;
+        let beginTranslateX = this.data.translateX;
+        let endTranslateX = - ((cardItemWidth + itemMargin) * index);
+
+        let isLock = this.data.isLock;
+        if (isLock) return;
+        this.data.isLock = true;
+
+        let teacherCardList = document.getElementById('teacher-card-list');
+        this.animateTo(beginTranslateX, endTranslateX, swiperDuration, (value) => {
+            teacherCardList.style.transform = `translateX(${value}px)`;
+        }, (value) => {
+
+            teacherCardList.style.transform = `translateX(${value}px)`;
+            this.data.index = index;
+            this.data.translateX = value;
+
+            this.data.isLock = false;
+        });
+    }
+
+    preArrow() {
+        let index = this.data.index;
+        this.goIndex(index - 1)
+    }
+
+    nextArrow() {
+        let index = this.data.index;
+        this.goIndex(index + 1);
+    }
+
+    playTime() {
+        let index = this.data.index;
+        let timer = setInterval(() => {
+            this.nextArrow();
+        }, 1500);
+        this.data.timer = timer;
+    }
+
+    pauseScroll() {
+        clearInterval(this.data.timer);
+        this.data.timer = null;
+    }
+
+}
+
+videoSrcList = ['https://q1.youked.jiker.com/course/3415/video-release/xl969-ke3415-ks10028-release-540p-20180806-b3yAetsuLpIBi8U7kcNSVbQWelB1jSjl47w84Kwg.mp4?e=1703507568&amp;token=7hbzf7Ut85UjN15uEc7f29qk1GfznmV8t-cYZHwn:sRyuIynv-p3JMsZs6HS46QFrFRA=',
+    'https://q1.youked.jiker.com/course/3416/video-release/xl969-ke3416-ks10029-release-720p-20180806-L8gKYMYXNKt6GJ85mtUCKsAhzkOYFKEwp1k8YP27.mp4?e=1703507568&token=7hbzf7Ut85UjN15uEc7f29qk1GfznmV8t-cYZHwn:M8K-LmPP_zEbK-rGFGiH0zE4yQA=',
+    'https://q1.youked.jiker.com/course/3416/video-release/xl969-ke3416-ks10030-release-540p-20180806-UMpqn1PnHhXToaFZ7s9DWN1KsfG2Dc23loXUChgY.mp4?e=1703507568&token=7hbzf7Ut85UjN15uEc7f29qk1GfznmV8t-cYZHwn:usM6pOkxoDEw7ByC2Xiqss4g6l8=',
+    'https://q1.youked.jiker.com/course/3416/video-release/xl969-ke3416-ks10031-release-720p-20180806-9u00fW5qYFMEzkZqZxCszs6enTf50PIu7keERQa7.mp4?e=1703507568&token=7hbzf7Ut85UjN15uEc7f29qk1GfznmV8t-cYZHwn:O5-S7u7DbTpMqv8raFqlLTp3CA0=',
+    'https://q1.youked.jiker.com/course/3417/video-release/xl969-ke3417-ks10032-release-540p-20180806-ZrtHk2M3IhPcREVuUASmTehKMQmHwACPnR7MctX8.mp4?e=1703507568&token=7hbzf7Ut85UjN15uEc7f29qk1GfznmV8t-cYZHwn:I-LT6ETFilgYQ4iS5qxDYh8FxtI=',
+    'https://q1.youked.jiker.com/course/3417/video-release/xl969-ke3417-ks10033-release-540p-20180806-gN0zRKrLAZT4joNB2RxZYdstegiE4U069Kp08tIE.mp4?e=1703507568&token=7hbzf7Ut85UjN15uEc7f29qk1GfznmV8t-cYZHwn:dZDTQmmKATmEruHfjexhTPD6zI4=',
+    'https://q1.youked.jiker.com/course/3417/video-release/xl969-ke3417-ks10034-release-720p-20180806-13CWdhVltQJ5qaLl4J74y8YwAVxrAe9apiOguQEc.mp4?e=1703507568&token=7hbzf7Ut85UjN15uEc7f29qk1GfznmV8t-cYZHwn:TGYduKvv4lkVKCt_Yh29B2-hIaw=',
+    'https://q1.youked.jiker.com/course/3418/video-release/xl969-ke3418-ks10035-release-720p-20180806-2g7TcYYtq4Sl7VhTQtIsXbNwaANpsTd9WZssISUc.mp4?e=1703507568&token=7hbzf7Ut85UjN15uEc7f29qk1GfznmV8t-cYZHwn:yhHJUSksFWuFgudyNPW9iXpQ-wE=',
+    'https://q1.youked.jiker.com/course/3418/video-release/xl969-ke3418-ks10036-release-720p-20180806-pyf4XhCFQgiJzh5IRLKUtwbIjmanrQVuB4mlepyG.mp4?e=1703507568&token=7hbzf7Ut85UjN15uEc7f29qk1GfznmV8t-cYZHwn:lWy3wIl8TWevONpl6p_ImD4B4QE=',
+    'https://q1.youked.jiker.com/course/3418/video-release/xl969-ke3418-ks10037-release-540p-20180806-erGipf2gFKLf2YfBiuBHyZEWltsZ2vrD5kgVeAUt.mp4?e=1703507568&token=7hbzf7Ut85UjN15uEc7f29qk1GfznmV8t-cYZHwn:nrcc6yM0R7amZTjKzIWy3B9jj_Q=',
+    'https://q1.youked.jiker.com/course/3418/video-release/xl969-ke3418-ks10038-release-720p-20180806-w3oHakqTb7uxLo32fYvffx6rEP85jHJxQzgGlofi.mp4?e=1703507568&token=7hbzf7Ut85UjN15uEc7f29qk1GfznmV8t-cYZHwn:GzlycZUXvw4dvFLmfjyyodPlM1I=',
+    'https://q1.youked.jiker.com/course/3418/video-release/xl969-ke3418-ks10039-release-540p-20180806-GccwMiE57SSnhOJ6cC5xXmQUnYIz1Cl0RKmZqCZB.mp4?e=1703507568&token=7hbzf7Ut85UjN15uEc7f29qk1GfznmV8t-cYZHwn:vS_sT8vT1deuVOnp0TVSi9TWnkU=',
+    'https://q1.youked.jiker.com/course/3418/video-release/xl969-ke3418-ks10040-release-540p-20180806-MWYgbbUFtGGo701h4M4knrxbX5prMRDkYDKRbmZV.mp4?e=1703507568&token=7hbzf7Ut85UjN15uEc7f29qk1GfznmV8t-cYZHwn:n59pbU4JexfYy7oCELSPbyb0AEQ=',];
+
+const PAGE = {
+    data: {
+        sectionArr: ["introduction-section", "course-section", "teacher-section", "product-section", "qualcomm-section"],
+        navItemActiveId: '',
+        navBarFixed: false,
+        wrapList: false,
+        navBarFixedOffset: [322.5, 74 + window.innerWidth * 71 / 320],
+        videoArr: videoSrcList,
+    },
+
+    init: function () {
+        this.initPage();
+        this.bind();
+    },
+
+    bind: function () {
+        window.addEventListener('scroll', this.refreshNav);
+
+        let navBar = document.getElementById('nav-bar');
+        this.onEventLister(navBar, 'click', 'nav-item', this.goNav);
+
+        let courseList = document.getElementById('course-list');
+        this.onEventLister(courseList, 'click', 'item-arrow', this.expandWrapList);
+        this.onEventLister(courseList, 'click', 'subitem-mask', this.heightLightSubItem);
+        this.onEventLister(courseList, 'click', 'subitem-mask', this.goVideo);
+    },
+
+    initPage: function () {
+        PAGE.swiper = new Swiper();
+    },
+
+    onEventLister: function (parentNode, action, childClassName, callback) {
+        parentNode.addEventListener(action, function (e) {
+            e.target.className.indexOf(childClassName) >= 0 && callback(e);
+        })
+    },
+
+    refreshNav: function () {
+        PAGE.data.navBarFixedOffset[1] = 74 + window.innerWidth * 71 / 320;
+        PAGE.fixedNav();
+        PAGE.heightLightNav();
+    },
+
+    fixedNav: function () {
+        let scrollTop = document.documentElement.scrollTop;
+        let viewWidth = window.innerWidth;
+        let fixedHeight = 0;
+        if (viewWidth <= 1120) {
+            fixedHeight = PAGE.data.navBarFixedOffset[0];
+        }
+        else {
+            fixedHeight = PAGE.data.navBarFixedOffset[1];
+        }
+        let navBarFixed = scrollTop > fixedHeight;
+        if (PAGE.data.navBarFixed !== navBarFixed) {
+            PAGE.data.navBarFixed = navBarFixed;
+            let navBar = document.getElementById('nav-bar');
+            let pageMainer = document.getElementById('page-mainer');
+            if (navBarFixed) {
+                navBar.className = 'header-nav fixed-top';
+                pageMainer.className = 'page-mainer relative'
+            }
+            else {
+                navBar.className = 'header-nav';
+                pageMainer.className = 'page-mainer'
+            }
+        }
+    },
+
+    heightLightNav: function () {
+        let scrollTop = document.documentElement.scrollTop;
+        let filterNav = PAGE.data.sectionArr.filter(data => {
+            let offsetTop = document.getElementById(data).offsetTop;
+            return scrollTop >= offsetTop - 70;
+        });
+        let navItemActiveId = filterNav.length ? filterNav[filterNav.length - 1] : '';
+        let pageHeight = document.documentElement.scrollHeight - window.innerHeight;
+        if (scrollTop >= pageHeight) { navItemActiveId = "qualcomm-section" }
+        if (PAGE.data.navItemActiveId !== navItemActiveId) {
+            PAGE.data.navItemActiveId = navItemActiveId;
+            let navItems = document.getElementsByClassName('nav-item');
+            for (let item of navItems) {
+                let dataNav = item.dataset.nav;
+                if (dataNav == navItemActiveId) {
+                    item.className = 'nav-item active';
+                }
+                else item.className = 'nav-item';
+            }
+        }
+    },
+
+    goNav: function (e) {
+        let id = e.target.dataset.nav;
+        let offsetTop = document.getElementById(id).offsetTop - 70;
+        document.documentElement.scrollTop = offsetTop + 1;
+    },
+
+    goVideo: function (e) {
+        let courseVideo = document.getElementById('course-video');
+        let videoOffSetTop = courseVideo.offsetTop;
+        document.documentElement.scrollTop = videoOffSetTop - 100;
+        let courseSubitem = e.target.parentNode;
+        let index = courseSubitem.dataset.index;
+        courseVideo.src = PAGE.data.videoArr[index];
+        courseVideo.play();
+    },
+
+    heightLightSubItem: function (e) {
+        let courseSubitem = e.target.parentNode;
+        let index = courseSubitem.dataset.index;
+        let courseSubitems = document.getElementsByClassName('course-subitem');
+        for (let item of courseSubitems) {
+            let dataIdex = item.dataset.index;
+            if (dataIdex == index) {
+                item.className = 'course-subitem active';
+            }
+            else item.className = 'course-subitem';
+        }
+    },
+
+    expandWrapList: function (e) {
+        let navArrow = e.target;
+        let courseItemLeft = navArrow.parentNode;
+        let courseItem = courseItemLeft.parentNode;
+        let courseSubitem = courseItem.nextElementSibling;
+        let wrapList = !PAGE.data.wrapList;
+        while (courseSubitem !== null) {
+            if (wrapList) {
+                courseSubitem.style.display = 'none';
+                courseSubitem = courseSubitem.nextElementSibling;
+                navArrow.className = 'item-arrow wrapped';
+            }
+            else {
+                courseSubitem.style.display = 'flex';
+                courseSubitem = courseSubitem.nextElementSibling;
+                navArrow.className = 'item-arrow';
+            }
+        }
+        PAGE.data.wrapList = wrapList;
+    },
+}
+
+PAGE.init();
+
---
+++ swiper-gaotong/js/swiper.js
@@ -0,0 +1,139 @@
+class Swiper {
+    constructor() {
+        this.data = {
+            index: 0,
+            duration: 500,
+            isLock: false,
+            translateX: 0,
+            defaultLength: null,
+            listWidth: null,
+            itemWidth: null,
+            itemMargin: 0,
+            timer: null,
+        }
+
+        this.nextArrow = this.nextArrow.bind(this);
+        this.preArrow = this.preArrow.bind(this);
+        this.init = this.init.bind(this);
+        this.playTime = this.playTime.bind(this);
+        this.pauseScroll = this.pauseScroll.bind(this);
+
+        this.init();
+        this.bind();
+    }
+
+    bind() {
+        let preArrow = document.getElementById('pre-arrow');
+        preArrow.addEventListener('click', this.preArrow);
+
+        let nextArrow = document.getElementById('next-arrow');
+        nextArrow.addEventListener('click', this.nextArrow);
+
+        //每1.5s自动切换，当鼠标移动到teacher-carousel里面，停止自动切换，当鼠标移出时，恢复自动切换
+        let teacherCarousel = document.getElementById('teacher-carousel');
+        teacherCarousel.addEventListener("mouseenter", this.pauseScroll);
+        teacherCarousel.addEventListener("mouseleave", this.playTime);
+
+        window.addEventListener('resize', this.init);
+    }
+
+    init() {
+        let teacherCardList = document.getElementById('teacher-card-list');
+        let teacherCards = document.getElementsByClassName('teacher-card');
+        let listWidth = teacherCardList.offsetWidth;
+        let cardItemWidth = teacherCards[0].offsetWidth;
+        let itemMargin = (listWidth - 4 * cardItemWidth) / 3;
+        for (let item of teacherCards) {
+            item.style.marginRight = itemMargin + 'px';
+        }
+        let index = this.data.index;
+        let translateX = - ((cardItemWidth + itemMargin) * index);
+        this.data.defaultLength = teacherCards.length - 4;
+        this.data.itemWidth = cardItemWidth;
+        this.data.itemMargin = itemMargin;
+        this.data.translateX = translateX;
+        teacherCardList.style.transform = `translateX(${translateX}px)`;
+
+        this.playTime();
+    }
+
+    animateTo(begin, end, duration, changeCallback, finishCallback) {
+        let startTime = Date.now();
+        let self = this;  // 将 this 存储在变量中
+        requestAnimationFrame(function update() {
+            let dateNow = Date.now();
+            let time = dateNow - startTime;
+            let value = self.linear(time, begin, end, duration);
+            typeof changeCallback == 'function' && changeCallback(value);
+
+            if (startTime + duration > dateNow) {
+                requestAnimationFrame(update);
+            } else {
+                typeof finishCallback === 'function' && finishCallback(end);
+            }
+        });
+    }
+
+    linear(time, begin, end, duration) {
+        return (end - begin) * time / duration + begin;
+    }
+
+    goIndex(index) {
+        let swiperDuration = this.data.duration;
+        let cardItemWidth = this.data.itemWidth;
+        let swiperLength = this.data.defaultLength;
+        let preArrow = document.getElementById('pre-arrow');
+        let nextArrow = document.getElementById('next-arrow');
+        if (index <= -1) {
+            index = swiperLength;
+        }
+        if (index >= swiperLength + 1) {
+            index = 0;
+        }
+
+
+        let itemMargin = this.data.itemMargin;
+        let beginTranslateX = this.data.translateX;
+        let endTranslateX = - ((cardItemWidth + itemMargin) * index);
+
+        let isLock = this.data.isLock;
+        if (isLock) return;
+        this.data.isLock = true;
+
+        let teacherCardList = document.getElementById('teacher-card-list');
+        this.animateTo(beginTranslateX, endTranslateX, swiperDuration, (value) => {
+            teacherCardList.style.transform = `translateX(${value}px)`;
+        }, (value) => {
+
+            teacherCardList.style.transform = `translateX(${value}px)`;
+            this.data.index = index;
+            this.data.translateX = value;
+
+            this.data.isLock = false;
+        });
+    }
+
+    preArrow() {
+        let index = this.data.index;
+        this.goIndex(index - 1)
+    }
+
+    nextArrow() {
+        let index = this.data.index;
+        this.goIndex(index + 1);
+    }
+
+    playTime() {
+        let index = this.data.index;
+        let timer = setInterval(() => {
+            this.nextArrow();
+        }, 1500);
+        this.data.timer = timer;
+    }
+
+    pauseScroll() {
+        clearInterval(this.data.timer);
+        this.data.timer = null;
+    }
+
+}
